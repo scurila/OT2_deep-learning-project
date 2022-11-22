0ba36c67fb94067c7befadde321964da6dfe6e4d
Stefania Curila <stefaniaurila@gmail.com>
2022-11-22 22:43:48 +0100

    rename + minor sliding window changes

--- scale_reduction.py
+++ scale_reduction.py
@@ -3,7 +3,7 @@ import imutils
 import cv2
 import time
 
-def pyramid(image, scale=1.5, minSize=(30, 30)):
+def pyramid(image, scale=1.25, minSize=(30, 30)): # rescale -20%
 	# yield the original image
 	yield image
 	# keep looping over the pyramid
@@ -23,25 +23,3 @@ def sliding_window(image, stepSize, windowSize):
 		for x in range(0, image.shape[1], stepSize):
 			# yield the current window
 			yield (x, y, image[y:y + windowSize[1], x:x + windowSize[0]])
-
-image = cv2.imread('family-portrait.jpg', cv2.IMREAD_GRAYSCALE)
-winW = 36
-winH = 36
-
-# loop over the image pyramid
-for resized in pyramid(image, scale=1.5):
-	# loop over the sliding window for each layer of the pyramid
-	for (x, y, window) in sliding_window(resized, stepSize=32, windowSize=(winW, winH)):
-		# if the window does not meet our desired window size, ignore it
-		if window.shape[0] != winH or window.shape[1] != winW:
-			continue
-		# THIS IS WHERE YOU WOULD PROCESS YOUR WINDOW, SUCH AS APPLYING A
-		# MACHINE LEARNING CLASSIFIER TO CLASSIFY THE CONTENTS OF THE
-		# WINDOW
-		# since we do not have a classifier, we'll just draw the window
-		clone = resized.copy()
-		cv2.rectangle(clone, (x, y), (x + winW, y + winH), (0, 255, 0), 2)
-		cv2.imshow("Window", clone)
-		
-		cv2.waitKey(1)
-		time.sleep(0.025)
--- sliding_window.py
+++ sliding_window.py
@@ -1,9 +1,24 @@
-import scale_reduction
+from scale_reduction import *
+from PIL import Image
+import os
+
+image = cv2.imread('family-portrait.jpg', cv2.IMREAD_GRAYSCALE)
+# size window
+winW = 36
+winH = 36
+
+# Remove all the contents of the cropped folder
+dir = 'cropped'
+for f in os.listdir(dir):
+    os.remove(os.path.join(dir, f))
+
+
+img_nb = 0 # index of the cropped image
 
 # loop over the image pyramid
-for resized in pyramid(image, scale=1.5):
+for resized in pyramid(image, scale=1.25):
 	# loop over the sliding window for each layer of the pyramid
-	for (x, y, window) in sliding_window(resized, stepSize=32, windowSize=(winW, winH)):
+	for (x, y, window) in sliding_window(resized, stepSize=6, windowSize=(winW, winH)):
 		# if the window does not meet our desired window size, ignore it
 		if window.shape[0] != winH or window.shape[1] != winW:
 			continue
@@ -12,7 +27,11 @@ for resized in pyramid(image, scale=1.5):
 		# WINDOW
 		# since we do not have a classifier, we'll just draw the window
 		clone = resized.copy()
+		crop_img = clone[y:y + winH, x:x + winW]
+		# print(crop_img)
+		Image.fromarray(crop_img,mode='L').save('cropped/img-cropped-'+str(img_nb)+'.jpg')
 		cv2.rectangle(clone, (x, y), (x + winW, y + winH), (0, 255, 0), 2)
 		cv2.imshow("Window", clone)
 		cv2.waitKey(1)
-		time.sleep(0.025)
+		#time.sleep(0.025)
+		img_nb += 1
