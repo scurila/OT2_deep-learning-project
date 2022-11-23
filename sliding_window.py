8ddb38a2cbc5f0ab796a71ca834ff0db06ef26d6
Stefania Curila <stefaniaurila@gmail.com>
2022-11-23 03:36:26 +0100

    tried to feed the NN witn the crops

--- sliding_window.py
+++ sliding_window.py
@@ -1,37 +1,72 @@
 from scale_reduction import *
 from PIL import Image
 import os
+import torch
+from net import *
+import torchvision
 
-image = cv2.imread('family-portrait.jpg', cv2.IMREAD_GRAYSCALE)
+image = cv2.imread('family-portrait-half.jpg', cv2.IMREAD_GRAYSCALE)
 # size window
 winW = 36
 winH = 36
 
-# Remove all the contents of the cropped folder
-dir = 'cropped'
-for f in os.listdir(dir):
-    os.remove(os.path.join(dir, f))
 
+def window_scale_reduction(net, winW, winH, image):
+	#net = net.float()
+	# Remove all the contents of the cropped folder
+	dir = 'cropped'
+	for f in os.listdir(dir):
+		os.remove(os.path.join(dir, f))
 
-img_nb = 0 # index of the cropped image
+	img_nb = 0 # index of the cropped image
 
-# loop over the image pyramid
-for resized in pyramid(image, scale=1.25):
-	# loop over the sliding window for each layer of the pyramid
-	for (x, y, window) in sliding_window(resized, stepSize=6, windowSize=(winW, winH)):
-		# if the window does not meet our desired window size, ignore it
-		if window.shape[0] != winH or window.shape[1] != winW:
-			continue
-		# THIS IS WHERE YOU WOULD PROCESS YOUR WINDOW, SUCH AS APPLYING A
-		# MACHINE LEARNING CLASSIFIER TO CLASSIFY THE CONTENTS OF THE
-		# WINDOW
-		# since we do not have a classifier, we'll just draw the window
-		clone = resized.copy()
-		crop_img = clone[y:y + winH, x:x + winW]
-		# print(crop_img)
-		Image.fromarray(crop_img,mode='L').save('cropped/img-cropped-'+str(img_nb)+'.jpg')
-		cv2.rectangle(clone, (x, y), (x + winW, y + winH), (0, 255, 0), 2)
-		cv2.imshow("Window", clone)
-		cv2.waitKey(1)
-		#time.sleep(0.025)
-		img_nb += 1
+	all_faces = []
+	# loop over the image pyramid
+	for resized in pyramid(image, scale=1.25):
+		faces = []
+		curr_scale_factor = image.shape[0] / resized.shape[0]
+		# loop over the sliding window for each layer of the pyramid
+		for (x, y, window) in sliding_window(resized, stepSize=6, windowSize=(winW, winH)):
+			# if the window does not meet our desired window size, ignore it
+			if window.shape[0] != winH or window.shape[1] != winW:
+				continue
+			
+			# THIS IS WHERE YOU WOULD PROCESS YOUR WINDOW, SUCH AS APPLYING A
+			# MACHINE LEARNING CLASSIFIER TO CLASSIFY THE CONTENTS OF THE
+			# WINDOW
+			window_tensor = torch.from_numpy(window)
+			# resize tensor from the shape [36, 36] to [1,1,36,36]
+			window_tensor = window_tensor[None, None, :, :]
+			output = net(window_tensor.float())
+			_, predicted = torch.max(output.data, 1)
+			if(predicted == torch.tensor([1])):
+				faces.append((x, y, output[0][1]))
+				crop_img = clone[y:y + winH, x:x + winW]
+				Image.fromarray(crop_img,mode='L').save('cropped/img-cropped-'+str(img_nb)+'.jpg')
+				img_nb += 1
+	
+			# since we do not have a classifier, we'll just draw the window
+			
+			clone = resized.copy()
+			#save the cropped image
+			#crop_img = clone[y:y + winH, x:x + winW]
+			#Image.fromarray(crop_img,mode='L').save('cropped/img-cropped-'+str(img_nb)+'.jpg')
+			cv2.rectangle(clone, (x, y), (x + winW, y + winH), (0, 255, 0), 2)
+			cv2.imshow("Window", clone)
+			cv2.waitKey(1)
+			#time.sleep(0.025)
+			#img_nb += 1
+
+		# Add the detected faces and the corresponding factors to the all_faces variable
+		all_faces.append([curr_scale_factor, faces])
+	# save the faces in a file
+	with open('detected_faces.txt', 'w') as fp:
+		for face in all_faces:
+			# write each item on a new line
+			fp.write("%s\n" % face)
+	return all_faces	
+
+if __name__ == "__main__":
+	net = Net()
+	net.load_state_dict(torch.load("./models/net_10.pth"))
+	window_scale_reduction(net, winW, winH, image)
--- test.py
+++ test.py
@@ -4,38 +4,39 @@ from net import Net
 import torchvision
 import torchvision.transforms as transforms
 
-PATH = './architectures/net_10.pth'
+PATH = './models/net_10.pth'
 
-net = Net()
-net.load_state_dict(torch.load(PATH))
+if __name__ == '__main__':
+    net = Net()
+    net.load_state_dict(torch.load(PATH))
 
-test_dir = './test_images'    # folder containing test images
+    test_dir = './test_images'    # folder containing test images
 
-transform = transforms.Compose(
-    [transforms.Grayscale(),   # transforms to gray-scale (1 input channel)
-     transforms.ToTensor(),    # transforms to Torch tensor (needed for PyTorch)
-     transforms.Normalize(mean=(0.5,),std=(0.5,))]) # subtracts mean (0.5) and devides by standard deviation (0.5) -> resulting values in (-1, +1)
-batch_size = 32    
+    transform = transforms.Compose(
+        [transforms.Grayscale(),   # transforms to gray-scale (1 input channel)
+        transforms.ToTensor(),    # transforms to Torch tensor (needed for PyTorch)
+        transforms.Normalize(mean=(0.5,),std=(0.5,))]) # subtracts mean (0.5) and devides by standard deviation (0.5) -> resulting values in (-1, +1)
+    batch_size = 32    
 
-test_data = torchvision.datasets.ImageFolder(test_dir, transform=transform)
-test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, shuffle=True, num_workers=1)
+    test_data = torchvision.datasets.ImageFolder(test_dir, transform=transform)
+    test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, shuffle=True, num_workers=1)
 
-correct = 0
-total = 0
-i = 0 # number of iterations
-print_every_n_batch = 100
-with torch.no_grad():
-    for data in test_loader:
-        images, labels = data
-        outputs = net(images)
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
+    correct = 0
+    total = 0
+    i = 0 # number of iterations
+    print_every_n_batch = 100
+    with torch.no_grad():
+        for data in test_loader:
+            images, labels = data
+            outputs = net(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
 
-        if i % print_every_n_batch == 0:
-            print(i)
-        i += 1
+            if i % print_every_n_batch == 0:
+                print(i)
+            i += 1
 
-print('Accuracy of the network on the 10 000 test images: %d %%' % (
-    100 * correct / total))
+    print('Accuracy of the network on the 10 000 test images: %d %%' % (
+        100 * correct / total))
 
